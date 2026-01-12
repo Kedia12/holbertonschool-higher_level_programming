e0377f1ce40bdd3c9b7c3e72f4cba06ef906e03c
Kedia12 <kediaihogoza01@gmail.com>
2026-01-12 04:46:35 +0000

    fix: added required space after the hashtag for the comment

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str1 = "Holberton" #first string
-str2 = "School" #second string
+str1 = "Holberton" # first string
+str2 = "School" # second string
 print(f"Welcome to {str1} {str2}!")
