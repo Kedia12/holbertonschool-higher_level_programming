49dbe2aa5a775a9987361c247bafc2f51a6ea407
Kedia12 <kediaihogoza01@gmail.com>
2026-01-19 13:22:20 +0000

    fix: used the format to prinnt each integer on its own line

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    list = my_list.reverse()
-    print("{:d}".format(list))
+   for n in reversed(my_list):
+    print("{:d}".format(n))
