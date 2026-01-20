e5ceef4e78e61cbebe98e5aec59ff2b9ba5f1a40
Kedia12 <kediaihogoza01@gmail.com>
2026-01-20 04:17:13 +0000

    fix: used the required format

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    for value in my_list:
+    new_list = my_list[:]
+    for value in new_list:
         if value % 2 == 0:
             return True
         else:
