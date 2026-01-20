74feaf49ca53d1deaa6ee9657a59af1bedaeb7ed
Kedia12 <kediaihogoza01@gmail.com>
2026-01-20 09:52:38 +0000

    fix: used the correct syntax to check for unique numbers

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
     set_3 = set()
-    for value in set_1 and set_2:
-        if value in set_1 == set_2:
-            continue
-        else:
+    for value in set_1:
+        if value not in set_2:
             set_3.add(value)
-            
+    for value in set_2:
+        if value not in set_1:
+            set_3.add(value)
+    return set_3
