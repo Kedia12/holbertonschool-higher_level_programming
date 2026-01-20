a563bfae7e741c00e5f1e21644463709a2b3e130
Kedia12 <kediaihogoza01@gmail.com>
2026-01-20 08:38:51 +0000

    fix: added indentation

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
+def search_replace(my_list, seach, replace):
     new_list = []
     for value in my_list:
         if value == search:
             new_list.append(replace)
         else:
             new_list.append(value)
-        return new_list
+    return new_list
