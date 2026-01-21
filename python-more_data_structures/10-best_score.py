84ff0fa8473d80bc6ad95ed02250571996c0a4a4
Kedia12 <kediaihogoza01@gmail.com>
2026-01-21 01:50:43 +0000

    feat: a function that returns a key with the biggest integer value

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    biggest_integer = a_dictionary[key]
+    for key in dictionary:
+        if key > biggest_integer:
+            biggest_integer = key
+    return biggest_integer
