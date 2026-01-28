c68a64bc396cc33698d05ce082f599969789b583
Kedia12 <kediaihogoza01@gmail.com>
2026-01-28 10:45:19 +0000

    fix: Implement Rectangle width/height getters and setters

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,9 +6,10 @@ class Rectangle:
     """Represents a rectangle."""
 
     def __init__(self, width=0, height=0):
-        """Initialize a rectangle with a private width and height attribute."""
-        self.__width = width
-    
+        """Initialize a Rectangle with optional width and height."""
+        self.width = width
+        self.height = height
+
     @property
     def width(self):
         """Retrieve the current width of the rectangle."""
@@ -16,21 +17,23 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """Set the width of the rectangle, with type and value validation."""
+        """Set the width of the rectangle with validation."""
         if type(value) is not int:
-            raise TypeError("Width must be an integer")
+            raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("Width must be >= 0")
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
-        """Retreive the current height of the rectangle."""
+        """Retrieve the current height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Set the height of the rectangle, with type and value validation."""
+        """Set the height of the rectangle with validation."""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
