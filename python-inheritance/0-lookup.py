976ace197e52b509f3d7ad7adfb6a044313704b9
Kedia12 <kediaihogoza01@gmail.com>
2026-02-03 02:23:28 +0000

    feat: a function that returns the list of available attributes and methods of an object

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,1 +1,7 @@
+#!/usr/bin/python3
+"""A function that lists an object's available attributes and methods."""
 
+
+def lookup(obj):
+    """Return the list of available attributes and methods of an object."""
+    return dir(obj)
