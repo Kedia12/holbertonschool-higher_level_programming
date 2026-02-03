eb8ab2532b1682eeb92fb4b47b648f752065cced
Kedia12 <kediaihogoza01@gmail.com>
2026-02-03 04:27:19 +0000

    feat: A method that raises an Exception with a message

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a BaseGeometry class."""
+
+
+class BaseGeometry:
+    """Represents a basegeometry."""
+
+    def area(self):
+        """Raise an Exception because area is not implemented."""
+        raise Exception("area() is not implemented")
