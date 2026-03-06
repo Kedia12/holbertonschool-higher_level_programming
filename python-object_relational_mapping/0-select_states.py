94cad0f80f564ac15eb916da4847abe9ce78de94
Kedia12 <kediaihogoza01@gmail.com>
2026-03-06 10:05:02 +0000

    fix: edited the host

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
-        host="localhost",
+        host="127.0.0.1",
         port=3306,
         user=argv[1],
         passwd=argv[2],
