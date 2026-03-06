8559d8a61827941392eae50c634ffafcdf7c9738
Kedia12 <kediaihogoza01@gmail.com>
2026-03-06 09:51:58 +0000

    fix: Added the missing shebang.

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-"""Lists all states from the database."""
+"""Lists all states from the specified MySQL database."""
 
 import MySQLdb
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
     )
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-    for row in cur.fetchall():
-        print(row)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    cur.close()
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+
+    cursor.close()
     db.close()
