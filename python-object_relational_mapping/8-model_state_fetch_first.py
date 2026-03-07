06eed0b3a59b1fee22a42c04826d36401d86654c
Kedia12 <kediaihogoza01@gmail.com>
2026-03-07 08:57:59 +0000

    Fix: used the right structure

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-"""Print the first State object from the database hbtn_0e_6_usa."""
+"""Print the first State object from the database."""
+
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-import sys
-
-
-def main():
-    """Define the main function"""
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}'
-                           '@localhost:3306/{}'.format(username, password,
-                                                       database))
-    session = sessionmaker(bind=engine)
-    session = session()
-    states = session.query(State).order_by(State.id).first()
-    if states:
-        print("{}: {}".format(states.id, states.name))
-    else:
-        print("Nothing")
-    session.close()
 
 
 if __name__ == "__main__":
-    main()
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).order_by(State.id).first()
+
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
+
+    session.close()
