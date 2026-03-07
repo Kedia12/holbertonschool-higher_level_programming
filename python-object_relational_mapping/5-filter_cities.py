887acdeae738204b55c3b8eb24a859f4d82a58e3
Kedia12 <kediaihogoza01@gmail.com>
2026-03-07 06:30:54 +0000

    fix: Remove extra spacing

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3
-"""A script that the argument and lists all cities."""
+/bin/python3
+"""List all cities of a given state."""
 
 import MySQLdb
 import sys
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
@@ -10,21 +11,21 @@ if __name__ == "__main__":
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        database=sys.argv[3]
-    }
-
-    cursor = database.cursor()
-
-    cursor.execute("""
-                   SELECT cities.id, cities.name, states.name
-                   FROM citiess JOIN states
-                   ON cities.state_id = states.id
-                   ORDER BY cities.id
-                   """, (sys.argv[4],)
+        db=sys.argv[3]
     )
 
-    for row in curl.fetchall():
-        print(row)
+    cur = db.cursor()
+    cur.execute(
+        """SELECT cities.name
+           FROM cities
+           JOIN states ON cities.state_id = states.id
+           WHERE states.name = %s
+           ORDER BY cities.id ASC""",
+        (sys.argv[4],)
+    )
+
+    rows = cur.fetchall()
+    print(", ".join(city[0] for city in rows))
 
     cur.close()
     db.close()
