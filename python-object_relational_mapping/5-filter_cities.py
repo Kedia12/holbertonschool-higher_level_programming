7403182677ebe17bd0e6e5010515ec234b4a4619
Kedia12 <kediaihogoza01@gmail.com>
2026-03-07 06:32:39 +0000

    fix: Added the correct command line

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,31 +1,33 @@
-/bin/python3
-"""List all cities of a given state."""
+#!/usr/bin/python3
+"""
+Module that list all cities of a state from a database.
+"""
 
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(
+
+    database = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3]
+        database=sys.argv[3]
     )
 
-    cur = db.cursor()
-    cur.execute(
-        """SELECT cities.name
-           FROM cities
-           JOIN states ON cities.state_id = states.id
-           WHERE states.name = %s
-           ORDER BY cities.id ASC""",
-        (sys.argv[4],)
-    )
+    cursor = database.cursor()
 
-    rows = cur.fetchall()
-    print(", ".join(city[0] for city in rows))
+    cursor.execute("""
+                   SELECT cities.name FROM cities
+                   JOIN states ON cities.state_id = states.id
+                   WHERE states.name LIKE BINARY %s
+                   ORDER BY cities.id
+                   """, (sys.argv[4],))
 
-    cur.close()
-    db.close()
+    results = cursor.fetchall()
+
+    print(", ".join(row[0] for row in results))
+
+    cursor.close()
+    database.close()
